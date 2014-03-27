6d3cf0df467ad4e1ad5abc0be741d52c3b09870e
Zachary Waldowski <zach@waldowski.me>
2014-03-27 17:26:40 -0400

    Fix missing headers in distribution

--- Mantle/Mantle.h
+++ Mantle/Mantle.h
@@ -13,8 +13,10 @@
 #import <Mantle/MTLValueTransformer.h>
 #import <Mantle/MTLTransformerErrorHandling.h>
 #import <Mantle/NSArray+MTLManipulationAdditions.h>
+#import <Mantle/NSError+MTLModelException.h>
 #import <Mantle/NSDictionary+MTLManipulationAdditions.h>
 #import <Mantle/NSDictionary+MTLMappingAdditions.h>
+#import <Mantle/NSDictionary+MTLJSONKeyPath.h>
 #import <Mantle/NSObject+MTLComparisonAdditions.h>
 #import <Mantle/NSValueTransformer+MTLInversionAdditions.h>
 #import <Mantle/NSValueTransformer+MTLPredefinedTransformerAdditions.h>
